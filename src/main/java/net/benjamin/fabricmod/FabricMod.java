b7022d2c7aef5e37c832b6e6d70f7299ce82ac09
Mew-10 <104922242+Mew-10@users.noreply.github.com>
2022-06-03 20:46:42 +0100

    Hey The Are Items

--- src/main/java/net/benjamin/fabricmod/FabricMod.java
+++ src/main/java/net/benjamin/fabricmod/FabricMod.java
@@ -1,16 +1,18 @@
 package net.benjamin.fabricmod;
 
+import net.benjamin.fabricmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class FabricMod implements ModInitializer {
-	public static final String MOD_ID = "tutorialmod";
+	public static final String MOD_ID = "fabricmod";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 
+		ModItems.registerModItems();
 
 	}
 }
