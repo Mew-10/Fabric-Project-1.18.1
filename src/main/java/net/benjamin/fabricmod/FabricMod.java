bdb4bdfd0c6484cd827a003dd604372ab78c8532
Mew-10 <104922242+Mew-10@users.noreply.github.com>
2022-06-03 20:33:48 +0100

    Initial commit

---
+++ src/main/java/net/benjamin/fabricmod/FabricMod.java
@@ -0,0 +1,16 @@
+package net.benjamin.fabricmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class FabricMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
